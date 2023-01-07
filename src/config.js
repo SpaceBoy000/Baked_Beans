9efd0f383fd07a0de6a16fa0faf284b1b192913a
spaceboy000 <cryptospaceboy00@gmail.com>
2023-01-07 14:53:24 +0300

    fix config file

--- src/config.js
+++ src/config.js
@@ -3,9 +3,3 @@ export const config = {
   contractAddress: "0x650382D51B1F10153d057895E56110bb6D3Bb1be", // "0x93Ed654824D7A5AF9D4F12b60A5238F7d66fc0C5", // "0x66d6a4191fb90d59E4BC782e3cEc3fCc8f5eD459",
   scanLink: "https://testnet.bscscan.com/address/0x650382D51B1F10153d057895E56110bb6D3Bb1be"
 };
-
-/******************************************************
-// Hi mo_cho                                          *
-// My Telegram ID: https://t.me/cryptospaceboy000     *
-// Skype: live:.cid.74cb1acb02746ef                   *
-/**************************************************** */
